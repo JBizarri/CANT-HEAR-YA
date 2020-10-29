73df65ab4c85b5d5b6a3be4ae67932c723508c27
Jefferson <jefferson.bizarri@gmail.com>
2020-10-28 21:27:55 -0300

    Reply with contact info and ignore urls

--- app/reply/ReplyToLowercase.py
+++ app/reply/ReplyToLowercase.py
@@ -22,20 +22,27 @@ MESSAGES = [
     "TIRA O COROTE DA BOCA E FALA DE NOVO",
     "PARANOIA MINHA OU ESSE DAÍ TÁ FALANDO MUITO BAIXO?",
     "EU JÁ SOU MOUCO DAÍ VC VEM E ME FALA BAIXO, AI NÃO MEU PARCEIRO",
-    "ALÔ??? ALÔ? NÃO TO OUVINDO!! QUEM É?!?!!??"
+    "ALÔ??? ALÔ? NÃO TO OUVINDO!! QUEM É?!?!!??",
 ]
 
 
 class ReplyToLowercase(ReplyBase):
     def __init__(self, subreddit) -> None:
-        self._allowed_words = [
-            r"\s?r/\w+$|r/\w+\s?|\s?r/\w+",
-            r"\s?u/\w+$|u/\w+\s?|\s?u/\w+",
-        ]
+        self._end_message = (
+            "EU SOU UM ROBÔ E ESSA AÇÃO FOI FEITA AUTOMATICAMENTE\n"
+            "[GITHUB](https://github.com/JBizarri/CANT-HEAR-YA) | [U/JEFFEWWASTAKEN](https://www.reddit.com/user/JeffewWasTaken)"
+        )
+
+        self._regexes = {
+            "urls": r"\((http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?\)",
+            "subreddits": r"\s?r/\w+$|r/\w+\s?|\s?r/\w+",
+            "users": r"\s?u/\w+$|u/\w+\s?|\s?u/\w+",
+        }
+
         super().__init__(subreddit)
 
     def _reply(self, comment):
-        processed_body = self._remove_allowed_words(comment.body)
+        processed_body = self._parse_comment(comment.body)
         if not self._is_uppercase(processed_body):
             print("Replying...")
             message = self._get_random_message()
@@ -43,7 +50,7 @@ class ReplyToLowercase(ReplyBase):
         else:
             print("OK")
 
-    def _remove_allowed_words(self, text: str):
+    def _parse_comment(self, text: str):
         """Remove words that are allowed to be lowercase
 
         Args:
@@ -52,10 +59,16 @@ class ReplyToLowercase(ReplyBase):
         Returns:
             # str: String without the allowed lowercase words
         """
-        for allowed_word in self._allowed_words:
-            text = re.sub(rf"{allowed_word}", "", text)
+        for regex in self._regexes.values():
+            text = re.sub(regex, "", text)
+        
+        text = text.replace('\n', "")          
 
         return text
+
+    def _get_random_message(self):
+        body = choice(MESSAGES)
+        return f"#{body}\n\n{self._end_message}"
 
     @staticmethod
     def _is_uppercase(text: str):
@@ -69,7 +82,3 @@ class ReplyToLowercase(ReplyBase):
         """
         # TODO: Make so if the majority of the text is uppercase return True
         return text.upper() == text
-
-    @staticmethod
-    def _get_random_message():
-        return choice(MESSAGES)
