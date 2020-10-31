641e48e63261f05f530338573fc163e16e8a7760
Jefferson <jefferson.bizarri@gmail.com>
2020-10-30 21:39:06 -0300

    Better logging and ignoring bot own comment

--- app/reply/ReplyBase.py
+++ app/reply/ReplyBase.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 from app.dao import delete_old_comments, fetch_comments, insert_comments
 from app.psql.Psql import Psql
+from app.config import USERNAME
 
 
 class ReplyBase(ABC):
@@ -18,36 +19,38 @@ class ReplyBase(ABC):
         for submission in self._subreddit.new(limit=self._limit):
             for comment in submission.comments.list():
 
+                if comment.author.name == USERNAME:
+                    continue
+
                 if comment.id in stored_comments:
                     new_comments[comment.id] = False
                     continue
 
-                print(f"Username: {comment.author.name}")
-                print(f"Comment preview: {comment.body[:50]}")
+                print(f"URL: https://www.reddit.com{comment.permalink}")
                 self._reply(comment)
 
                 new_comments[comment.id] = True
-                
+
         self._delete_old_comments(new_comments)
         self._insert_comments(new_comments)
-    
+
     @abstractmethod
     def _reply(self, comment):
         pass
-    
+
     def _fetch_comments(self):
         print("Fetching Comments' IDs from DB")
         result = fetch_comments(self._psql)
         if result is None:
             return list()
-        
+
         return [comment[0] for comment in result]
-    
+
     def _delete_old_comments(self, new):
         result = delete_old_comments(self._psql, tuple(new.keys()))
         if not result:
-            print(f"Deleted old Comments' IDs status message: {result}")
-  
+            print(f"Delete old Comments' IDs status message: {result}")
+
     def _insert_comments(self, comments):
         param = [(id_,) for id_, new in comments.items() if new]
         if param:
